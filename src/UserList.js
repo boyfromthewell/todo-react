62335bf4657563bc48b63241ebb4135e3965aa9d
kwon <tnsdyd10@naver.com>
2021-11-05 17:55:24 +0900

    useEffect

--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle, onModify }) {
+  useEffect(() => {
+    console.log("user값이 설정됨");
+    console.log(user);
+    return () => {
+      console.log("user가 바뀌기전..");
+      console.log(user);
+    };
+  }, [user]);
+/*
+useEffect
+첫번째 파라미터 : 함수
+두번째 파라미터: 의존값들어있는 배열(deps)
+deps 배열 비우면 컴포넌트 처음 나타날때만 useEffect 등록 함수 호출
+
+함수 반환가능 - cleanup 함수(useEffect에 대해 뒷정리를 해줌), deps 비어있는 경우 
+컴포넌트 사라질때 cleanup 호출
+
+deps에 특정값 넣으면
+컴포너니트 처음 마운트 될때도 호출, 지정값 바뀔때도 호출
+언마운트때도 호출, 값 바뀌기 직전에도 호출
+
+useEffect 안에서 사용하는 상태나, 프롭스가 있다면 useEffect deps안에 넣어주기
+
+deps 파라미터 생략하면, 컴포넌트 리렌더링 될때마다 호출
+*/
   return (
     <div>
       <b
