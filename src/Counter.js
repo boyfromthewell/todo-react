f015fcc33d0e4a47cfaacefe6cc9cb68d5a5ef01
kwon <tnsdyd10@naver.com>
2021-11-17 01:43:41 +0900

    class component

--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,34 @@
-import React, { useReducer } from "react";
+import React, { Component } from "react";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
+class Counter extends Component {
+  state = {
+    //상태 관리:state사용
+    counter: 0,
+    fixed: 1,
+  };
+  handleIncrease = () => {
+    this.setState((state) => ({
+      //상태 업데이트 : this.setState 함수 사용
+      counter: state.counter + 1,
+    }));
+    this.setState((state) => ({
+      counter: state.counter + 1, //함수형 업데이트로 처리해주면 값이 2씩 더해짐
+    }));
+  };
+  handleDecrease = () => {
+    this.setState((state) => ({
+      counter: state.counter - 1,
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
+        <p>고정된 값: {this.state.fixed}</p>
+      </div>
+    );
   }
 }
-function Counter() {
-  const [number, dispatch] = useReducer(reducer, 0);
-
-  const onIncrease = () => {
-    dispatch({ type: "INCREMENT" });
-  };
-  const onDecrease = () => {
-    dispatch({ type: "DECREMENT" });
-  };
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-    </div>
-  );
-}
-
 export default Counter;
--- src/Hello.js
+++ src/Hello.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Hello({ color, name, isSpecial }) {
-  return (
-    <div style={{ color }}>
-      {isSpecial ? <b>*</b> : null}
-      안녕하세요 {name}
-    </div>
-  );
+class Hello extends Component {
+  render() {
+    const { color, name, isSpecial } = this.props;
+    return (
+      <div style={{ color }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
+    );
+  }
 }
-
 Hello.defaultProps = {
-  name: "이름없음",
+  name: "이름 없음",
 };
+
 export default Hello;
+
+//클래스형 컴포넌트 (잘 쓰지는 않음)
